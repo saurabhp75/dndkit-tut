e2dc4e146a54f54949a7fc76d8fb028b2e8e7536
Saurabh Prakash <saurabhp75@gmail.com>
2024-09-13 04:35:18 +0530

    Add dnd-kit sortable list

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,8 +1,21 @@
+import { closestCenter, DndContext, type DragEndEvent } from '@dnd-kit/core'
+import {
+	arrayMove,
+	SortableContext,
+	useSortable,
+	verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 import { type MetaFunction } from '@remix-run/node'
 import { useState } from 'react'
 import { Button } from '#app/components/ui/button.js'
 import { Input } from '#app/components/ui/input.js'
 import { data } from './data'
+
+type User = {
+	id: number
+	name: string
+}
 
 export const meta: MetaFunction = () => [{ title: 'Epic Notes' }]
 
@@ -21,8 +34,23 @@ export default function Index() {
 		setInputValue('')
 		setUsers((prev) => [...prev, newUser])
 	}
+
+	function onDragEnd(e: DragEndEvent) {
+		// console.dir(e)
+		const { active, over } = e
+		if (!over || active.id === over.id) {
+			return
+		}
+		setUsers((users) => {
+			const oldIndex = users.findIndex((user) => user.id === active.id)
+			const newIndex = users.findIndex((user) => user.id === over.id)
+			// We have used a helper to swap the two entries in the users list
+			return arrayMove(users, oldIndex, newIndex)
+		})
+	}
+
 	return (
-		<main className="font-poppins flex flex-col gap-4 h-full justify-center items-center">
+		<main className="font-poppins flex h-full flex-col items-center justify-center gap-4">
 			<div>Total: {users.length}</div>
 			<div className="flex gap-3">
 				<Input
@@ -30,15 +58,41 @@ export default function Index() {
 					value={inputValue}
 					onChange={(e) => setInputValue(e.target.value)}
 				/>
-				<Button onClick={addUser}>Add user</Button>
+				<Button size="lg" onClick={addUser}>
+					Add user
+				</Button>
 			</div>
 			<div>
-				{users.map((user) => (
-					<div className="mb-1 border px-2" key={user.id}>
-						{user.name}
-					</div>
-				))}
+				<DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+					<SortableContext items={users} strategy={verticalListSortingStrategy}>
+						{users.map((user) => (
+							<SortableUser key={user.id} user={user} />
+						))}
+					</SortableContext>
+				</DndContext>
 			</div>
 		</main>
+	)
+}
+
+function SortableUser({ user }: { user: User }) {
+	const { attributes, listeners, setNodeRef, transform, transition } =
+		useSortable({ id: user.id })
+
+	const style = {
+		transition,
+		transform: CSS.Transform.toString(transform),
+	}
+
+	return (
+		<div
+			ref={setNodeRef}
+			style={style}
+			{...attributes}
+			{...listeners}
+			className="mb-1 border px-2"
+		>
+			{user.name}
+		</div>
 	)
 }
